40b069e8815f96903a15f32db978a306b31d940b
ruppjo01 <ruppjo01@luther.edu>
2015-10-17 20:39:45 -0500

    Update to_do.js

--- to_do.js
+++ to_do.js
@@ -1,3 +1,16 @@
-additem = function() {
+addTask = function() {
+  myList = document.querySelector("#list");
+  task = document.querySelector("#task").value;
+  li = document.createElement("li");
+  
+  checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  
+  category = document.querySelector("#priority");
+  
+  text = document.createTextNode(task);
+  li.appendChild(text)
+  myList.appendChild(li)
+  
   
 }
